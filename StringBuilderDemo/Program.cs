4ebc3c7be40dde788b187b2b84a5c6b2d8fae10b
aayushhman <aayushmandal696@gmail.com>
2025-02-12 15:39:19 +0545

    string and stringbuilder differnce;

--- StringBuilderDemo/Program.cs
+++ StringBuilderDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace StringBuilderDemo
 {
@@ -10,6 +11,27 @@ namespace StringBuilderDemo
     {
         static void Main(string[] args)
         {
+          string s1 = "Aayush";
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                s1 = s1 + i;
+               
+            }
+            sw1.Stop();
+            StringBuilder sb = new StringBuilder();
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append(i);
+            }
+            sw2.Stop();
+            Console.WriteLine("Time taken by string:" + sw1.ElapsedMilliseconds);
+            Console.WriteLine("Time taken by stringBuilder:" + sw2.ElapsedMilliseconds);
+
+            Console.ReadLine();
         }
     }
 }
